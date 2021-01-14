721d7d5634c514355e6e729da4a9369ce05e3996
sameteren <samet.eren@inomera.com>
2021-01-15 01:13:26 +0300

    initial commit

---
+++ customer-services/src/main/java/com/motivici/customerservices/MotiviciApplication.java
@@ -0,0 +1,13 @@
+package com.motivici.customerservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotiviciApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotiviciApplication.class, args);
+	}
+
+}
---
+++ customer-services/src/test/java/com/motivici/customerservices/MotiviciApplicationTests.java
@@ -0,0 +1,13 @@
+package com.motivici.customerservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotiviciApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
